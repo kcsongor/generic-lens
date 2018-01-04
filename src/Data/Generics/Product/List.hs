4352f7199e2916784ae106d6abf1a745454bcbec
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2018-01-04 15:41:34 +0100

    Add constraint synonyms for working with records specifically

--- src/Data/Generics/Product/List.hs
+++ src/Data/Generics/Product/List.hs
@@ -21,12 +21,15 @@
 
 module Data.Generics.Product.List
   ( IsList (..)
+  , IsRecord
+  , IsRecord'
   ) where
 
 import Data.Generics.Internal.Lens
 import Data.Generics.Product.Internal.List
 import Data.Kind
 import GHC.Generics
+import GHC.TypeLits
 
 class IsList
   (m :: Type)
@@ -42,3 +45,31 @@ instance
   , GIsList m (Rep f) (Rep g) as bs
   ) => IsList m f g as bs where
   list = repIso . (glist @m)
+
+class IsList Symbol f g as bs => IsRecord f g (as :: [(Symbol, Type)]) (bs :: [(Symbol, Type)]) | f -> as, g -> bs
+instance IsList Symbol f g as bs => IsRecord f g as bs
+
+class IsRecord f f as as => IsRecord' f (as :: [(Symbol, Type)]) | f -> as
+instance IsRecord f f as as => IsRecord' f as
+
+-- example (TODO: move elsewhere)
+--class PrintRecord (rl :: [(Symbol, Type)]) where
+--  printRecord' :: List rl -> String
+--
+--instance PrintRecord '[] where
+--  printRecord' _ = ""
+--
+--instance (KnownSymbol field, Show a, PrintRecord xs) => PrintRecord ('(field, a) ': xs) where
+--  printRecord' (x :> xs) = show x ++ ", " ++ printRecord' xs
+--
+--printRecord :: (IsRecord' rec rl, PrintRecord rl) => rec -> String
+--printRecord rec = printRecord' (rec ^. list)
+--
+--data MyRecord = MyRecord
+--  { field1 :: Int
+--  , field2 :: String
+--  , field3 :: Bool
+--  } deriving Generic
+--
+-- >>> printRecord (MyRecord 10 "hello" False)
+-- "10, \"hello\", False, "
