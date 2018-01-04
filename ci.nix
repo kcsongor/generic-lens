f680cc88de4e44f1fba6eee44eaff196d1cb9066
Matthew Pickering <matthewtpickering@gmail.com>
2018-01-04 15:37:39 +0000

    WIP: Try using nix for CI

---
+++ ci.nix
@@ -0,0 +1,35 @@
+let
+  overlay = self: super:
+          {
+            simple-server = super.callPackage ./simple-serve/server.nix {};
+
+            # An attribute which contains the head overrides.
+            patches = super.callPackage ./head.hackage/scripts/overrides.nix
+              { patches = ./head.hackage/patches; };
+
+            ghc802 = super.haskell.packages.ghc802;
+            ghc822 = super.haskell.packages.ghc822;
+
+
+            # A modified package set intented to be used with ghcHEAD
+            ghcHEAD = super.haskell.packages.ghcHEAD.override
+              { overrides = sel: sup:
+                  ((super.callPackage self.patches {} sel sup)
+                  // { mkDerivation = drv: sup.mkDerivation
+                        ( drv // { jailbreak = true; doHaddock = false;});
+                        generic-deriving = super.haskell.lib.dontCheck sup.generic-deriving;
+                       });
+                   };
+
+            all-cabal-hashes = super.fetchurl {
+            url    = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e1089e56e666c2a0fe82f840d3cc7f49b9c9fe9b.tar.gz";
+            sha256 = "0qbzdngm4q8cmwydnrg7jvipw39nb1mjxw95vw6f789874002kn1";
+            };
+          };
+
+  nixpkgs = import <nixpkgs> { overlays = [overlay]; };
+in
+  { pkgSet ? "ghc802" }:
+  nixpkgs.${pkgSet}.callCabal2nix "generic-lens" ./. {}
+
+
