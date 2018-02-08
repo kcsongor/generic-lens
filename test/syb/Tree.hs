a623316a36591907a4752b59bbeaf4aa8817a5b1
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2018-02-08 16:35:31 +0000

    More interesting examples

--- test/syb/Tree.hs
+++ test/syb/Tree.hs
@@ -10,7 +10,7 @@ Example adapted from SYB
 
 module Main (main) where
 
-import Control.Lens.Fold (toListOf)
+import Control.Lens
 import Control.Monad (void)
 import Data.Generics.Product
 import GHC.Generics
@@ -23,12 +23,25 @@ main = void $ runTestTT tests
 data Tree a w = Leaf a
               | Fork (Tree a w) (Tree a w)
               | WithWeight (Tree a w) w
-       deriving (Show, Generic)
+       deriving (Show, Generic, Eq)
 
 -- A typical tree
 mytree :: Tree Int Int
 mytree = Fork (WithWeight (Leaf 42) 1)
               (WithWeight (Fork (Leaf 88) (Leaf 37)) 2)
+
+mytreeShown :: Tree String Int
+mytreeShown = Fork (WithWeight (Leaf "42") 1)
+                (WithWeight (Fork (Leaf "88") (Leaf "37")) 2)
+
+-- A polymorphic-recursive structure
+data Poly a b
+  = PNil
+  | PCons a (Poly b a)
+  deriving (Show, Generic)
+
+poly :: Poly Int String
+poly = PCons 10 (PCons "hello" (PCons 20 (PCons "world" PNil)))
 
 -- Print everything like an Int in mytree
 -- In fact, we show two attempts:
@@ -36,9 +49,22 @@ mytree = Fork (WithWeight (Leaf 42) 1)
 --   2. print everything wrapped with Leaf
 -- So (1.) confuses leafs and weights whereas (2.) does not.
 tests :: Test
-tests = show ( toListOf (typesDeep @Int) mytree
-             , toListOf (param @1) mytree
-             ) ~=? output
+tests = TestList
+  [ toListOf (typesDeep @Int) mytree ~=? [42,1,88,37,2]
+  , toListOf (param @1) mytree       ~=? [42,88,37]
+
+  -- Things not (easily) doable in SYB:
+  -- change type of Tree by mapping a function over the second (from the right) param
+  , (mytree & param @1 %~ show)                           ~=? mytreeShown
+  -- collect values in poly corresponding to the first param
+  , toListOf (param @0) poly                              ~=? ["hello", "world"]
+  -- collect all Ints inside poly
+  , toListOf (typesDeep @Int) poly                        ~=? [10, 20]
+  -- map length over the Strings, then collect all Ints
+  , toListOf (typesDeep @Int) (poly & param @0 %~ length) ~=? [10, 5, 20, 5]
+  -- map length over the Strings, then collect all the resulting Ints
+  , toListOf (param @0) (poly & param @0 %~ length)       ~=? [5,5]
+  ]
 
 -- original code from SYB:
 --tests = show ( listify (\(_::Int) -> True)         mytree
@@ -48,6 +74,3 @@ tests = show ( toListOf (typesDeep @Int) mytree
 --    fromLeaf :: Tree Int Int -> [Int]
 --    fromLeaf (Leaf x) = [x]
 --    fromLeaf _        = []
-
-output :: String
-output = "([42,1,88,37,2],[42,88,37])"
