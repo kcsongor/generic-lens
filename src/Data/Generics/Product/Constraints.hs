8191cfd444877e701b0345d7e30d92b23484b515
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2018-02-27 20:57:48 +0000

    Simplify typesDeep implementation

--- src/Data/Generics/Product/Constraints.hs
+++ src/Data/Generics/Product/Constraints.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE AllowAmbiguousTypes   #-}
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE KindSignatures        #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE Rank2Types            #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE AllowAmbiguousTypes     #-}
+{-# LANGUAGE ConstraintKinds         #-}
+{-# LANGUAGE DefaultSignatures       #-}
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE KindSignatures          #-}
+{-# LANGUAGE MonoLocalBinds          #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE Rank2Types              #-}
+{-# LANGUAGE ScopedTypeVariables     #-}
+{-# LANGUAGE TypeApplications        #-}
+{-# LANGUAGE UndecidableInstances    #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 -----------------------------------------------------------------------------
 -- |
@@ -26,6 +30,7 @@ module Data.Generics.Product.Constraints
     --
     --  $example
     HasConstraints (..)
+  , Deep
   ) where
 
 import Data.Generics.Product.Internal.Types
@@ -37,9 +42,23 @@ import Data.Generics.Internal.VL.Traversal
 class HasConstraints (c :: * -> Constraint) s where
   constraints :: Applicative g => (forall a. c a => a -> g a) -> s -> g s
 
+  default constraints :: Applicative g => (forall a. c a => a -> g a) -> s -> g s
+  constraints _ = pure
+
 instance
   ( Generic s
   , GHasConstraints c (Rep s)
   ) => HasConstraints c s where
   constraints = confusingC @c (\f s -> to <$> gconstraints @c f (from s))
   {-# INLINE constraints #-}
+
+instance {-# OVERLAPPING #-} HasConstraints c Bool
+instance {-# OVERLAPPING #-} HasConstraints c Char
+instance {-# OVERLAPPING #-} HasConstraints c Double
+instance {-# OVERLAPPING #-} HasConstraints c Float
+instance {-# OVERLAPPING #-} HasConstraints c Int
+instance {-# OVERLAPPING #-} HasConstraints c Integer
+instance {-# OVERLAPPING #-} HasConstraints c Ordering
+
+class (c a, HasConstraints (Deep c) a) => Deep c a
+instance (c a, HasConstraints (Deep c) a) => Deep c a
