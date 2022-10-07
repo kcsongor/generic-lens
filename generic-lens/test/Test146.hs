0ee2a6ef0707f6f77f7be4b55342d83b0a973cca
Luke Worth <luke.worth@bellroy.com>
2022-10-07 15:45:34 +1100

    Add instance AsType a a
    Resolves #146

---
+++ generic-lens/test/Test146.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Test146 where
+
+import Control.Monad.Except
+import Data.Generics.Sum
+import GHC.Generics
+
+data Error = Error
+  deriving (Generic)
+
+poly :: (AsType Error e, MonadError e m) => m ()
+poly = undefined
+
+mono :: ExceptT Error IO ()
+mono = poly
+
+
